95f9e119ea20813f17ad0d03a5e51303995d4965
“Seu <oclesianok@gmail.com>
2026-07-13 22:14:11 -0300

    commit

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 import express from "express"
-
+ 
+ 
 const app = express()
-// server ou app 
-//Configura uma rota para o metodo HTTP GET na raiz ("/") do aplicativo
-app.get("/api/v1/somar", (req, res) => {
+ 
+app.use(express.json())
+ 
 
-console.log(req.query)
-
-    const num1 = Number(req.query.num1)
-    const num2 =  Number(req.query.num2)
-    const resultado = num1 + num2 
-    //Retorna uma resposta JSON contendo uma mensagem "Hello Word"
-    res.send({ resultado })
+app.post("/api/v1/ex1", (req, res) => {
+    const {num1, num2} = req.body
+const resultado = Number(num1) + Number(num2)
+ 
+res.status(200).send({message: resultado})
 })
-//Inicia o servidor Express na porta 3000 e define uma função de retorno de chamada
-app.listen(3000, () => {
-    //Exbice uma mensagem no console quando o servidor estiver ativo.
-    console.log("servidor ouvindo na porta 3000")
+
+app.post("/api/v1/ex3", (req, res) =>{
+const {p1, p2, p3, p4, p5, peso} = req.body
+const resultado = Number(p1 + p2 + p3 + p4 + p5) / Number(peso)
+ 
+res.status(200).send({message: resultado})
+})
+
+app.post("/api/v1/ex2", (req, res) => {
+    const {hora, salario} = req.body
+    const resultado = Number(hora) * Number(salario)
+ 
+    res.send({message: resultado})
+})
+app.post("/api/v1/ex4", (req, res) => {
+    const {celsius} = req.body
+    const fahrenheit = Number((9 * (celsius) + 160) / 5) 
+ 
+    res.send({message: fahrenheit})
+
+})
+
+    app.post("/api/v1/ex5", (req, res) =>{
+const {milhas} = req.body
+const quilometro = milhas = 1.60934  
+ 
+res.status(200).send({message: quilometro})
+})
+
+ app.listen(3000, () => {
+    console.log("Servidor Rodando")
 })
